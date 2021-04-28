a9bd4409d549b042d7ba15ad79e80374520be25e
CtrlVaultDel <ctrlvaultdel51@gmail.com>
2021-04-28 15:12:53 -0500

    Added SQL file to produce local DB for GroSharies

---
+++ 01_Db_Create.sql.sql
@@ -0,0 +1,86 @@
+﻿Use [master]
+
+IF db_id('GroSharies') IS NULL
+	CREATE DATABASE [GroSharies]
+GO
+
+USE [GroSharies]
+GO
+
+DROP TABLE IF EXISTS [ListItem];
+DROP TABLE IF EXISTS [Purchase];
+DROP TABLE IF EXISTS [ShoppingList];
+DROP TABLE IF EXISTS [Household];
+DROP TABLE IF EXISTS [UserType];
+DROP TABLE IF EXISTS [HouseholdUser];
+DROP TABLE IF EXISTS [User];
+GO
+
+CREATE TABLE [Household] (
+  [Id] integer PRIMARY KEY IDENTITY,
+  [Name] nvarchar(50) NOT NULL
+)
+GO
+
+CREATE TABLE [User] (
+  [Id] integer PRIMARY KEY IDENTITY,
+  [FirebaseId] nvarchar(28) NOT NULL,
+  [Email] nvarchar(50) NOT NULL,
+  [FirstName] nvarchar(50) NOT NULL,
+  [LastName] nvarchar(50) NOT NULL,
+
+  CONSTRAINT UQ_FirebaseId UNIQUE(FirebaseId)
+)
+GO
+
+CREATE TABLE [UserType] (
+  [Id] integer PRIMARY KEY IDENTITY,
+  [Name] nvarchar(50) NOT NULL
+)
+GO
+
+CREATE TABLE [ShoppingList] (
+  [Id] integer PRIMARY KEY IDENTITY,
+  [HouseholdId] integer NOT NULL,
+  [Name] nvarchar(50) NOT NULL,
+  [DateCreated] datetime NOT NULL,
+
+  CONSTRAINT [FK_ShoppingList_Household] FOREIGN KEY ([HouseholdId]) REFERENCES [Household] ([Id]) 
+)
+GO
+
+CREATE TABLE [Purchase] (
+  [Id] integer PRIMARY KEY IDENTITY,
+  [ShoppingListId] integer NOT NULL,
+  [UserId] integer NOT NULL,
+  [Vendor] nvarchar(50) NOT NULL,
+  [PurchaseDate] datetime NOT NULL,
+  [TotalCost] decimal NOT NULL,
+
+  CONSTRAINT [FK_Purchase_ShoppingList] FOREIGN KEY ([ShoppingListId]) REFERENCES [ShoppingList] ([Id]), 
+  CONSTRAINT [FK_Purchase_User] FOREIGN KEY ([UserId]) REFERENCES [User] ([Id]) 
+)
+GO
+
+CREATE TABLE [ListItem] (
+  [Id] integer PRIMARY KEY IDENTITY,
+  [ShoppingListId] integer NOT NULL,
+  [Name] nvarchar(50) NOT NULL,
+  [IsChecked] bit NOT NULL DEFAULT 0,
+
+  CONSTRAINT [FK_ListItem_ShoppingList] FOREIGN KEY ([ShoppingListId]) REFERENCES [ShoppingList] ([Id])
+)
+GO
+
+CREATE TABLE [HouseholdUser] (
+  [Id] integer PRIMARY KEY IDENTITY,
+  [HouseholdId] integer NOT NULL,
+  [UserId] integer NOT NULL,
+  [UserTypeId] integer NOT NULL,
+  [IsAccepted] bit NOT NULL DEFAULT 0,
+
+  CONSTRAINT [FK_HouseholdUser_Household] FOREIGN KEY ([HouseholdId]) REFERENCES [Household] ([Id]),
+  CONSTRAINT [FK_HouseholdUser_User] FOREIGN KEY ([UserId]) REFERENCES [User] ([Id]),
+  CONSTRAINT [FK_HouseholdUser_UserType] FOREIGN KEY ([UserTypeId]) REFERENCES [UserType] ([Id])
+)
+GO
