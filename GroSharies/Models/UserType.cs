c4dceb4e12b89a32c7fe7cf6a11f9d36fbf5e200
CtrlVaultDel <ctrlvaultdel51@gmail.com>
2021-04-29 10:52:32 -0500

    Added calculated field DisplayName to User Model. Added UserType Model

---
+++ GroSharies/Models/UserType.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace GroSharies.Models
+{
+    public class UserType
+    {
+        public int Id { get; }
+
+        public string FirebaseId { get;}
+    }
+}
