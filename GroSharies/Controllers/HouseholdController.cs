f7d2c5b2775b1a4694d040e1e61651510460e7f7
CtrlVaultDel <ctrlvaultdel51@gmail.com>
2021-04-29 13:49:36 -0500

    Finished with backend code needed to attain households for a specific user

--- GroSharies/Controllers/HouseholdController.cs
+++ GroSharies/Controllers/HouseholdController.cs
@@ -2,6 +2,7 @@
 using Microsoft.AspNetCore.Mvc;
 using GroSharies.Models;
 using GroSharies.Repositories;
+using System.Security.Claims;
 
 namespace GroSharies.Controllers
 {
@@ -11,9 +12,35 @@ namespace GroSharies.Controllers
     public class HouseholdController : ControllerBase
     {
         private readonly IHouseholdRepository _householdRepository;
-        public HouseholdController(IHouseholdRepository householdRepository)
+        private readonly IUserRepository _userRepository;
+
+        public HouseholdController(
+            IHouseholdRepository householdRepository, 
+            IUserRepository userRepository)
         {
             _householdRepository = householdRepository;
+            _userRepository = userRepository;
+        }
+
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            // Get the current user by using the FirebaseId
+            var user = GetCurrentUser();
+
+            // If no user is returned, do not continue
+            if (user == null) return NotFound();
+
+            // Get the Id and Name of each Household the user is in and return it
+            var userHouseholds = _householdRepository.GetAll(user.Id);
+            return Ok(userHouseholds);
+        }
+
+        // Retrieves the current user object by using the provided firebaseId
+        private User GetCurrentUser()
+        {
+            var firebaseId = User.FindFirst(ClaimTypes.NameIdentifier).Value;
+            return _userRepository.GetByFirebaseId(firebaseId);
         }
     }
 }
