b1c96c4c208b0a45f7d7d65623787b793c5dc982
CtrlVaultDel <ctrlvaultdel51@gmail.com>
2021-04-29 10:58:44 -0500

    Added Household Model

---
+++ GroSharies/Models/Household.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace GroSharies.Models
+{
+    public class Household
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [MaxLength(50)]
+        public string Name { get; set; }
+    }
+}
