57967f0deb898baf11b386e7e1b9974d2c3f6c3d
CtrlVaultDel <ctrlvaultdel51@gmail.com>
2021-04-29 11:15:42 -0500

    Added Purchase model

---
+++ GroSharies/Models/Purchase.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace GroSharies.Models
+{
+    public class Purchase
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public int ShoppingListId { get; set; }
+
+        [Required]
+        public int UserId { get; set; }
+
+        [Required]
+        [MaxLength(50)]
+        public string Vendor { get; set; }
+
+        [Required]
+        public DateTime DateCreated { get; set; }
+
+        [Required]
+        public decimal TotalCost { get; set; }
+    }
+}
