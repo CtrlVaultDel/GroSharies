f21d58e8bfa15277de3385d8fa542b238c37da3a
CtrlVaultDel <ctrlvaultdel51@gmail.com>
2021-05-04 11:23:18 -0500

    Fixed Edit and Delete buttons for HouseholdPage & ShoppingLists so they don't look like a 2 year old made them

--- GroSharies/client/src/components/ShoppingLists/ShoppingList.js
+++ GroSharies/client/src/components/ShoppingLists/ShoppingList.js
@@ -1,13 +1,43 @@
-import React from "react";
-import { Card, CardHeader } from "reactstrap";
+import React, { useContext } from "react";
+import { Card, CardHeader, CardFooter, Button, Row, Col } from "reactstrap";
 import { Link } from "react-router-dom";
+import { FaRegEdit } from "react-icons/fa";
+import { FaTrashAlt } from "react-icons/fa";
+import { ShoppingListContext } from "../../providers/ShoppingListProvider";
 
 const ShoppingList = ({ shoppingList }) => {
+    const { deleteShoppingList } = useContext(ShoppingListContext);
+
+    const deleteWarning = () => {
+        const confirmBox = window.confirm(`Are you sure you wish to delete the ${shoppingList.name} shopping list? This action is irreversable.`);
+        if (confirmBox){
+            deleteShoppingList(shoppingList.id);
+        };
+    };
+
     return (
         <Card className="m-2 shadow postCard">
-            <CardHeader>
+
+            {/* Header & Link to ShoppingList Detail */}
+            <CardHeader >
                 <Link to={`/shoppingList/${shoppingList.id}`}>{shoppingList.name}</Link>
             </CardHeader>
+            <CardFooter>
+                <Row>
+                    <Col className="text-center">
+                        {/* Edit button for ShoppingList */}
+                        <Button size="sm">
+                            <FaRegEdit>
+                                <Link to={{pathname:`/shoppingList/edit/${shoppingList.id}`, state:{householdId: shoppingList.householdId}}} />
+                            </FaRegEdit>
+                        </Button>
+                    </Col>
+                    <Col className="text-center">
+                        {/* Delete button for ShoppingList */}
+                        <Button size="sm" onClick={() => deleteWarning()}><FaTrashAlt /></Button>
+                    </Col>
+                </Row>
+            </CardFooter>
         </Card>
     );
 };
