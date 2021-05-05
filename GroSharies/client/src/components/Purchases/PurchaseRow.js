17886929bcc1003f45f2c05065d76a1f176483a0
CtrlVaultDel <ctrlvaultdel51@gmail.com>
2021-05-04 20:36:30 -0500

    Converted date to normal date: mm/dd/yyyy

--- GroSharies/client/src/components/Purchases/PurchaseRow.js
+++ GroSharies/client/src/components/Purchases/PurchaseRow.js
@@ -1,10 +1,12 @@
 import React from "react";
 
 const PurchaseRow = ({ purchaseDetail }) => {
+    const date = new Date(purchaseDetail.purchase.purchaseDate)
+
 
     return(
        <tr>
-           <td>{purchaseDetail.purchase.purchaseDate}</td>
+           <td>{date.toLocaleDateString("en-US")}</td>
            <td>{purchaseDetail.buyer}</td>
            <td>{purchaseDetail.purchase.vendor}</td>
            <td>{purchaseDetail.purchase.totalCost}</td>
