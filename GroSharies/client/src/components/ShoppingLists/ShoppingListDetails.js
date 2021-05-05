4bf1ca955e122e73df505f0daee96d2ff43027a2
CtrlVaultDel <ctrlvaultdel51@gmail.com>
2021-05-05 11:20:17 -0500

    Refactored ShoppingListDetails to handle setting state better

--- GroSharies/client/src/components/ShoppingLists/ShoppingListDetails.js
+++ GroSharies/client/src/components/ShoppingLists/ShoppingListDetails.js
@@ -5,38 +5,28 @@ import {ShoppingListContext} from "../../providers/ShoppingListProvider";
 import PurchaseRow from "../Purchases/PurchaseRow";
 
 const ShoppingListDetails = () => {
+    const [shoppingList, setShoppingList] = useState([]);
+    const [checkedItems, setCheckedItems] = useState([]);
+    const [uncheckedItems, setUncheckedItems] = useState([]);
+    const [purchases, setPurchases] = useState([]);
     const { getShoppingList } = useContext(ShoppingListContext);
 
-    /*
-    shoppingListDetails = {
-        shoppingList,
-        listItems,
-        purchases
-    }
-    */
-    // shopListDet == shoppingListDetails
-    const [ shopListDet, setShopListDet] = useState()
     const { id } = useParams();
-
-    useEffect(() => {
-        getShoppingList(id)
-        .then(setShopListDet);
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-    }, []);
-    
-    if(shopListDet == null) return null;
-    
-    // Destructure the ShoppingListDetails object
-    const { shoppingList, listItems, purchases } = shopListDet;
 
     const getCheckedItems = items => items.filter(li => li.isChecked === true);
     const getUncheckedItems = items => items.filter(li => li.isChecked === false);
 
-    const checkedItems = getCheckedItems(listItems);
-    const uncheckedItems = getUncheckedItems(listItems);
-
-    const handleChange = item => console.log("Item Changed", item.name, item.isChecked);
-
+    useEffect(() => {
+        getShoppingList(id)
+        .then(resp => {
+            setShoppingList(resp.shoppingList);
+            setCheckedItems(getCheckedItems(resp.listItems));
+            setUncheckedItems(getUncheckedItems(resp.listItems));
+            setPurchases(resp.purchases)
+        });
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, []);
+    
     return (
         <Container>
             <Row className="justify-content-md-center">
